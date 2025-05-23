b6ab8a086d4cbcde181281a5a98a6ff9b254747d
RohanAdus <78970151+RohanAdus@users.noreply.github.com>
2025-05-23 16:34:10 -0400

    Update mochihealth.py

--- mochihealth.py
+++ mochihealth.py
@@ -3,7 +3,7 @@
 from pyngrok import ngrok
 
 # replace with your real token from https://dashboard.ngrok.com/get-started/your-authtoken
-ngrok.set_auth_token("2xVhcqimbGsjuSGTJ1gXPj1tJH3_4k7GjNFej7ZrQFJfB9db7")
+ngrok.set_auth_token("")
 
 # 2) Launch your app
 get_ipython().system_raw("streamlit run app.py &")
